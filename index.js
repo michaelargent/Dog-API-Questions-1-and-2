541b269be0706ffe7a86dd2662ec48961526aef8
michaelargent <53536596+michaelargent@users.noreply.github.com>
2019-11-18 09:47:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,31 +2,31 @@
 
 function getDogImage(numberOfDogs) {
     fetch(`https://dog.ceo/api/breeds/image/random/${numberOfDogs}`)
-      .then(response => response.json())
-      .then(responseJson => 
-        displayResults(responseJson))
-      .catch(error => alert('Something went wrong. Try again later.'));
-  };
+        .then(response => response.json())
+        .then(responseJson => 
+              displayResults(responseJson))
+        .catch(error => alert('Something went wrong. Try again later.'));
+};
   
-    function displayResults(responseJson) {
-        console.log(responseJson);
-        $(".results").html("");
-        responseJson.message.forEach(renderedImg => {
-          $(".results").append(`<img src="${renderedImg}" class="results">`);
-        });
-        //display the results section
-        $(".results").removeClass("hidden");
-      };
+function displayResults(responseJson) {
+    console.log(responseJson);
+    $(".results").html("");
+    responseJson.message.forEach(renderedImg => {
+        $(".results").append(`<img src="${renderedImg}" class="results">`);
+    });
+    //display the results section
+    $(".results").removeClass("hidden");
+};
 
 function watchSubmit() {
-  $('form').submit(event => {
-    event.preventDefault();
-    let numberOfDogs = document.getElementById("numberOfDogs").value;
-    getDogImage(numberOfDogs);
-  });
+    $('form').submit(event => {
+        event.preventDefault();
+        let numberOfDogs = document.getElementById("numberOfDogs").value;
+        getDogImage(numberOfDogs);
+    });
 };
 
 $(function() {
-  console.log('App loaded! Waiting for submit!');
-  watchSubmit();
+    console.log('App loaded! Waiting for submit!');
+    watchSubmit();
 });
